cf90aacad381960eeacc5607544983eb2e4017d1
slee7 <slee@catalina7ventures.com>
2021-06-21 22:10:10 -0700

    practice with comparison operators

--- app.py
+++ app.py
@@ -11,14 +11,25 @@
 #     print(name + ", You old..")
 
 
-price = 1000000
-buyer_credit = input("How is your credit? (Good or Bad?)").lower()
+# price = 1000000
+# buyer_credit = input("How is your credit? (Good or Bad?)").lower()
 
-if buyer_credit == "good":
-    down = 0.1
-    down_payment = str(price * down)
+# if buyer_credit == "good":
+#     down = 0.1
+#     down_payment = str(price * down)
+# else:
+#     down = 0.2
+#     down_payment = str(price * down)
+
+# print("Your down payment will be " + down_payment)
+
+
+high_income = False
+good_credit = False
+
+if high_income and good_credit:
+    print("You are eligible for a loan with a good rate.")
+elif high_income or good_credit:
+    print("You are eligible for a loan under circumstances")
 else:
-    down = 0.2
-    down_payment = str(price * down)
-
-print("Your down payment will be " + down_payment)
+    print("You are not eligible for a loan.")
