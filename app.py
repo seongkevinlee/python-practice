1391fc330df0b2628bbb3504bb07333c9a9f1948
slee7 <slee@catalina7ventures.com>
2021-06-22 23:10:15 -0700

    created guessing number game

--- app.py
+++ app.py
@@ -53,3 +53,15 @@ while i <= 5:
     print('*' * i)
     i += 1
 print("Done.")
+
+secret_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int(input("Guess a number: "))
+    guess_count += 1
+    if guess == secret_number:
+        print("You win!")
+        break
+else:
+    print("You lose!")
