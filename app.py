fb7c78402f061700cc42c4a79c7f683cec90c634
slee7 <slee@catalina7ventures.com>
2021-06-22 23:35:14 -0700

    finished car-game

--- app.py
+++ app.py
@@ -48,20 +48,47 @@ import math
 
 
 # WHILE LOOPS
-i = 1
-while i <= 5:
-    print('*' * i)
-    i += 1
-print("Done.")
+# i = 1
+# while i <= 5:
+#     print('*' * i)
+#     i += 1
+# print("Done.")
 
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input("Guess a number: "))
-    guess_count += 1
-    if guess == secret_number:
-        print("You win!")
+# secret_number = 9
+# guess_count = 0
+# guess_limit = 3
+# while guess_count < guess_limit:
+#     guess = int(input("Guess a number: "))
+#     guess_count += 1
+#     if guess == secret_number:
+#         print("You win!")
+#         break
+# else:
+#     print("You lose!")
+
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
+    if command == "start":
+        if started:
+            print("Car is already on!")
+        else:
+            started = True
+            print("Car started...")
+    elif command == "stop":
+        if not started:
+            print("Car is already stopped!")
+        else:
+            started = False
+            print("Car stopped.")
+    elif command == "help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to quit
+        """)
+    elif command == "quit":
         break
-else:
-    print("You lose!")
+    else:
+        print("Sorry, unrecognized command.")
