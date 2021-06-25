8b32a67df1305cf8ca26ae7054863cf9f04c8a83
slee7 <slee@catalina7ventures.com>
2021-06-24 22:07:21 -0700

    created 2d list or matrix

--- lists.py
+++ lists.py
@@ -3,9 +3,23 @@
 # print(names)
 
 #! PROGRAM TO FIND THE LARGEST NUMBER IN A LIST
-list = [34, 64, 83, 13, 47, 463, 473, 457]
-max = list[0]
-for number in list:
-    if number > max:
-        max = number
-print(max)
+# list = [34, 64, 83, 13, 47, 463, 473, 457]
+# max = list[0]
+# for number in list:
+#     if number > max:
+#         max = number
+# print(max)
+
+#! 2D Lists
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+# matrix[1][1] = 20
+# print(matrix)
+# print(matrix[1][1])
+
+for row in matrix:
+    for item in row:
+        print(item)
