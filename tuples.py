f6c3a87c90a49ef3c1daff415747e18406ec8f9d
slee7 <slee@catalina7ventures.com>
2021-06-29 23:15:26 -0700

    practice unpacking tuples and lists

--- tuples.py
+++ tuples.py
@@ -1,6 +1,16 @@
 #! tuples are immuteable
-numbers = (1, 2, 3, 3, 5, 7, 9)
-print(numbers[0])
+# numbers = (1, 2, 3, 3, 5, 7, 9)
+# print(numbers[0])
 
-print(numbers.count(3))
-print(numbers.index(5))
+# print(numbers.count(3))
+# print(numbers.index(5))
+
+
+coordinates = (1, 2, 3)
+# x = cooridinates[0]
+# y = cooridinates[1]
+# z = cooridinates[2]
+
+x, y, z = coordinates
+
+print(x * y * z)
