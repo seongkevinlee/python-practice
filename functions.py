3bb4855dcdeb758be073ac0dc17854745a50c55a
slee7 <slee@catalina7ventures.com>
2021-07-07 22:37:47 -0700

    practicing parameters of functions

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
-def greet_user():
-    print('Hi there!')
+def greet_user(name):
+    print(f'Hi {name}!')
     print('Welcome Aboard!')
 
 
 print('Start!')
-greet_user()
+greet_user("John")
+greet_user("Mary")
 print('Finish!')
