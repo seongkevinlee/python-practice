f108e010d655e2ef7b034476d06918880985d0c6
slee7 <slee@catalina7ventures.com>
2021-07-07 22:41:31 -0700

    created multiples parameters with input variable arguments

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-def greet_user(name):
+def greet_user(name, age):
     print(f'Hi {name}!')
-    print('Welcome Aboard!')
+    print(f'You are {age} years old!')
 
 
-print('Start!')
-greet_user("John")
-greet_user("Mary")
-print('Finish!')
+name = input('What is your name? ')
+age = input("How old are you? ")
+
+
+greet_user(name, age)
